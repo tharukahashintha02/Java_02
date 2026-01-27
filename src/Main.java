7a2888bd38ca544f9b4d5072b54d81831cbb86f7
tharuka2002 <wthdilshan@students.nsbm.ac.lk>
2026-01-27 11:40:40 +0530

    create simple email check with substrings

---
+++ src/Main.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args){
+
+        Scanner scanner = new Scanner(System.in);
+
+        String email;
+        String username;
+        String domain;
+
+        System.out.print("Enter your email : ");
+        email = scanner.nextLine();
+
+        if(email.contains("@")){
+            username = email.substring(0,email.indexOf("@"));
+            domain = email.substring(email.indexOf("@") + 1);
+
+            System.out.println(username);
+            System.out.println(domain);
+        }
+        else{
+            System.out.println("your email is not valid , try again");
+        }
+
+
+        scanner.close();
+
+    }
+}
